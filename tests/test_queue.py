6b49c2b0c2931f0fa9005e2328fbcbf8b83b1fd2
VladimirBek <vladimirbek1996@gmail.com>
2023-05-25 18:04:13 +0300

    added tests for methods enqueue, __str__ in queue.py

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,27 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Queue
+
+
+class TestStack(unittest.TestCase):
+
+    def test_enqueue(self):
+        q1 = Queue()
+        q1.enqueue(1)
+        self.assertEqual(q1.head.data, 1)
+        q1.enqueue(2)
+        self.assertEqual(q1.tail.data, 2)
+        self.assertEqual(q1.head.data, 1)
+        q1.enqueue(3)
+        self.assertEqual(q1.head.next_node.data, 2)
+        self.assertEqual(q1.tail.data, 3)
+        self.assertEqual(q1.head.data, 1)
+
+    def test_str(self):
+        q1 = Queue()
+        q1.enqueue(1)
+        q1.enqueue(2)
+        q1.enqueue(3)
+        self.assertEqual(str(q1), '1\n2\n3')
+        q1.enqueue(4)
+        self.assertEqual(str(q1), '1\n2\n3\n4')
