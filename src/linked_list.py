408fe4bd2b8ce853923ad6baff94b9d043c08c69
VladimirBek <vladimirbek1996@gmail.com>
2023-06-06 15:27:14 +0300

    added methods get_data_by_id, to_list added try/except blocks in methods insert_beginning, insert_at_end

--- src/linked_list.py
+++ src/linked_list.py
@@ -21,20 +21,48 @@ class LinkedList:
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-
-        if self.head is None:
-            self.head = self.tail = Node(data)
-        else:
-            self.head = Node(data, next_node=self.head)
+        try:
+            if not isinstance(data, dict) or data.get('id') == None:
+                raise TypeError('Данные не являются словарем или в словаре нет id.')
+            if self.head is None:
+                self.head = self.tail = Node(data)
+            else:
+                self.head = Node(data, next_node=self.head)
+        except TypeError:
+            print('Данные не являются словарем или в словаре нет id.')
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        node = Node(data)
-        if self.head is None:
-            self.head = self.tail = node
-        else:
-            self.tail.next_node = node
-            self.tail = node
+        try:
+            if not isinstance(data, dict) or data.get('id') == None:
+                raise TypeError('Данные не являются словарем или в словаре нет id.')
+            node = Node(data)
+            if self.head is None:
+                self.head = self.tail = node
+            else:
+                self.tail.next_node = node
+                self.tail = node
+        except TypeError:
+            print('Данные не являются словарем или в словаре нет id.')
+
+    def to_list(self):
+        res = []
+        node = self.head
+        if node is None:
+            return res
+
+        while node:
+            res.append(node.data)
+            node = node.next_node
+        return res
+
+    def get_data_by_id(self, id_val):
+        node = self.head
+        while node:
+            if node.data['id'] == id_val:
+                return node.data
+            node = node.next_node
+        return f'В списке нет элемента с таким id'
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
